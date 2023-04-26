e3ef840009f26966e966feb1dc2a124b31fea5ce
watitwa <markwatitwa98@gmail.com>
2023-04-26 19:28:18 +0300

    update links

--- artisan/urls.py
+++ artisan/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='artisan-home'),
     path('dashboard', views.dashboard, name='artisan-dashboard')
 ]
