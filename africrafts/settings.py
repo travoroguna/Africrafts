5892328e3b3e3f794c6d17f2441e1113b8a95a76
watitwa <markwatitwa98@gmail.com>
2023-04-26 14:17:40 +0300

    configure template

--- africrafts/settings.py
+++ africrafts/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-39r^g9+_wl-1!=*-_p5%a*v49hhk*_mgr_9n%h2bej^lab4nf2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -113,6 +113,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
