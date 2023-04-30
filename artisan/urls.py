602de43cc2274c6ba623b8f251e61413a9ccb97b
watitwa <markwatitwa98@gmail.com>
2023-05-01 00:49:03 +0300

    clean up dashboard

--- artisan/urls.py
+++ artisan/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from django.shortcuts import redirect
 
 urlpatterns = [
-    path('', views.home, name='artisan-home'),
+    path('', lambda request: redirect('artisan-dashboard'), name='artisan-home'),
     path('dashboard', views.dashboard, name='artisan-dashboard')
 ]
--- artisan/views.py
+++ artisan/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from shop.models import Customer
+from artisan.models import Product
 
 # Create your views here.
-def home(request):
-    return render(request, 'artisan_index.html')
 
 
 @login_required()
 def dashboard(request):
     context = {
-        'segment': 'dashboard'
+        'segment': 'dashboard',
+        'customers': Customer.objects.all(),
+        'products': Product.objects.all()
     }
     return render(request, 'artisan_dashboard.html', context)
