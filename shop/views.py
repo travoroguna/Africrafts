f45dcf206a34f6263f7b7777708ec5efe962b18d
watitwa <markwatitwa98@gmail.com>
2023-05-01 02:35:05 +0300

    list products

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from artisan.models import Product
 
 
 # Create your views here.
 def shop(request):
-    return render(request, 'shop_index.html')
+    context = {
+        'products': Product.objects.all()
+    }
+    return render(request, 'shop_index.html', context)
