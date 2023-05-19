fa60b182fd409707c8701c31a77a2b26afe44efc
MercyWanjohi <mercy.wanjohi@kibo.school>
2023-05-19 16:12:36 +0300

    about us page
    Co-authored-by: Travor oguna <travoroguna@users.noreply.github.com>

--- landing/urls.py
+++ landing/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landing, name='landing')
+    path('', views.landing, name='landing'),
+    path('about', views.about_us, name= 'about_us')
 ]
--- landing/views.py
+++ landing/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def landing(request):
     return render(request, 'index.html')
+
+def about_us(request):
+    return render(request,'about_us.html')
